ecdc75e15fb409fc3063f6b516b35a82fb61388b
addisuanley2010 <addisuanley2005@gmail.com>
2024-04-17 08:09:44 +0300

    image API fixed

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='h-screen flex justify-center items-center '>
+    <div className='h-96 flex justify-center items-center '>
       <div className='animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-gray-900'>
 
       </div>
--- src/utl/config.js
+++ src/utl/config.js
@@ -1,5 +1,4 @@
-// require("dotenv").config();
 
-// const BASE_URL = process.env.BASE_URL;
 
 export const BASE_URL = "https://my-blog-backend-mnpb.onrender.com/api";
+// export const BASE_URL="http://localhost:3001/api"
